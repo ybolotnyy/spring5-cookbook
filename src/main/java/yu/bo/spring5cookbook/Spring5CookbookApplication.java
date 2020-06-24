8255c29858542baa6c0ec73dfbef88d76de015b3
ybolotnyy <yuriy.bolotnyy@gmail.com>
2020-06-24 15:42:21 -0700

    Create Spring 5 project

---
+++ src/main/java/yu/bo/spring5cookbook/Spring5CookbookApplication.java
@@ -0,0 +1,13 @@
+package yu.bo.spring5cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5CookbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5CookbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yu/bo/spring5cookbook/Spring5CookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.bo.spring5cookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5CookbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
