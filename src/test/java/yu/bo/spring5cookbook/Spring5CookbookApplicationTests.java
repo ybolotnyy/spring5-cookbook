c8d56543d6a60a494eb5b51d48932dd1b1495780
ybolotnyy <yuriy.bolotnyy@gmail.com>
2020-06-24 21:36:41 -0700

    Add Category test

--- src/test/java/yu/bo/spring5cookbook/Spring5CookbookApplicationTests.java
+++ src/test/java/yu/bo/spring5cookbook/Spring5CookbookApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Spring5CookbookApplicationTests {
 
     @Test
+    //Integration test for checking if the App content loads on start
     void contextLoads() {
     }
 
---
+++ src/test/java/yu/bo/spring5cookbook/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package yu.bo.spring5cookbook.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+    }
+}
