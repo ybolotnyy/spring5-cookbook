b7d19081064e16a9e89049033d8e6a71b9bf52cc
ybolotnyy <yuriy.bolotnyy@gmail.com>
2020-06-25 12:42:51 -0700

    Add tests for CategoryRepository

--- src/test/java/yu/bo/spring5cookbook/repositories/CategoryRepositoryTest.java
+++ src/test/java/yu/bo/spring5cookbook/repositories/CategoryRepositoryTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class UnitOfMeasureRepositoryTest {
+public class CategoryRepositoryTest {
 
     @Autowired
     CategoryRepository categoryRepository;
